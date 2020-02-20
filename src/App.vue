77bcccebe12aafdbf6b4861d5fb8a133563f92fc
onitwin <onitwin@gmail.com>
2020-02-20 11:52:26 +0000

    basic list displaying

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Brewdog</h1>
+    <list-of-beers :beers="beers"></list-of-beers>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {eventBus} from './main.js';
+import BeerList from './components/BeerList.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{
+      beers:[],
+      selectedBeer:null
+    }
+  },
+  mounted(){
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(res =>res.json())
+    .then(data => this.beers=data)
+
+  eventBus.$on('beer-selected')
+  },
+  components:{
+    "list-of-beers":BeerList
+  },
 }
 </script>
 
---
+++ src/components/BeerList.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div>
+  <ul>
+    <list-item v-for="(beer,index) in beers" :beer="beer" :key="index"></list-item>
+  </ul>
+</div>
+
+</template>
+
+<script>
+import BeerItem from './BeerItem.vue'
+export default {
+  name:'list-of-beers',
+  props:['beers'],
+  components:{
+    "list-item":BeerItem
+  }
+}
+</script>
+
+<style lang="css" scoped>
+li{
+  list-style-type: none;
+}
+</style>
